4c5f012d6c96c2e96612d3cc3e423c697db5e0f1
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-07-15 23:45:36 +0530

    Create variables.js

---
+++ Learn-JavaScript/Basics/variables.js
@@ -0,0 +1,36 @@
+
+let x = 89;
+let a; // declaration
+a = 78; // assignment
+console.log(a);
+
+//Data Type
+//Number
+let age = 16;
+let price = 10.99;
+let gpa = 8.85
+console.log(typeof age);
+console.log(`Your GPA is ${gpa}`);
+console.log(`Your age is ${age} `);
+
+//String
+let firstName = "Code"
+let lastName = "Vibes"
+let favFruit = "Mango"
+let email = "code@gmail.com"
+
+console.log(typeof firstName);
+console.log(`My name is ${firstName} ${lastName} and my favourite fruit is ${favFruit}`);
+console.log(`My email id is ${email}`);
+
+//Boolean
+let online = true;
+let isJob = false;
+console.log(typeof online);
+console.log(`Am i online? ${online}`);
+console.log(`Do I have a job? ${isJob}`);
+
+const pi = 3.14
+let r = 4
+let circ = 2*pi*r;
+console.log(circ);
